1c17df4ac77946c6abc01feaaa521a735be15dac
kkKaan <kaan.karacanta@gmail.com>
2025-03-23 21:47:19 +0300

    closest pair distance problem

--- algorithms/07-divide_and_conquer/closest_pair/README.md
+++ algorithms/07-divide_and_conquer/closest_pair/README.md
@@ -2,19 +2,47 @@
 
 ## Overview
 
+Given a set of points in the plane, find the closest pair of points.
 
-
-- Time complexity: 
-- Space complexity: 
+- Time complexity: $O(n \log n)$
+- Space complexity: $O(n)$
 
 ## How It Works
 
-1. 
+1. Sort the points according to their x-coordinates.
+2. Divide the set of points into two equal-sized subsets by a vertical line.
+3. Solve the problem recursively in the two subsets.
+4. Check if there is a pair of points, one in each subset, that are closer than the smallest distance found in the recursive calls.
+5. Combine the results.
 
 ## Pseudocode
 ```plaintext
+closest_pair(points)
+    sort points by x-coordinate
+    return closest_pair_recursive(points)
+
+closest_pair_recursive(points)
+    if |points| <= 3
+        return brute_force(points)
+    
+    mid = |points| / 2
+    left = points[0:mid]
+    right = points[mid:]
+    
+    left_closest = closest_pair_recursive(left)
+    right_closest = closest_pair_recursive(right)
+    
+    closest = min(left_closest, right_closest)
+    
+    mid_x = points[mid].x
+    strip = []
+    for point in points
+        if |point.x - mid_x| < closest
+            strip.append(point)
+    
+    return min(closest, strip_closest(strip))
 ```
 
 ## Example Implementations
 
-- 
+- [Closest Pair of Points in C++](./closest_pair.cpp)
--- algorithms/07-divide_and_conquer/closest_pair/closest_pair.cpp
+++ algorithms/07-divide_and_conquer/closest_pair/closest_pair.cpp
@@ -1,13 +1,20 @@
+#include <cfloat>
 #include <iostream>
 
 using namespace std;
 
-void merge(int* nums, int l, int m, int r)
+struct Point
+{
+    int x;
+    int y;
+};
+
+void merge(Point* nums, int l, int m, int r, int index)
 {
     int n1 = m - l + 1;
     int n2 = r - m;
 
-    int leftArr[n1], rightArr[n2];
+    Point leftArr[n1], rightArr[n2];
     for (int i = 0; i < n1; ++i)
     {
         leftArr[i] = nums[l + i];
@@ -22,13 +29,27 @@ void merge(int* nums, int l, int m, int r)
 
     while (leftIndex < n1 && rightIndex < n2)
     {
-        if (leftArr[leftIndex] < rightArr[rightIndex])
+        if (index == 0)
         {
-            nums[l++] = leftArr[leftIndex++];
+            if (leftArr[leftIndex].x < rightArr[rightIndex].x)
+            {
+                nums[l++] = leftArr[leftIndex++];
+            }
+            else
+            {
+                nums[l++] = rightArr[rightIndex++];
+            }
         }
         else
         {
-            nums[l++] = rightArr[rightIndex++];
+            if (leftArr[leftIndex].y < rightArr[rightIndex].y)
+            {
+                nums[l++] = leftArr[leftIndex++];
+            }
+            else
+            {
+                nums[l++] = rightArr[rightIndex++];
+            }
         }
     }
 
@@ -42,26 +63,116 @@ void merge(int* nums, int l, int m, int r)
     }
 }
 
-void merge_sort(int* nums, int l, int r)
+void merge_sort(Point* nums, int l, int r, int index)
 {
     if (l < r)
     {
         int midIndex = (l + r) / 2;
-        merge_sort(nums, l, midIndex);
-        merge_sort(nums, midIndex + 1, r);
-        merge(nums, l, midIndex, r);
+        merge_sort(nums, l, midIndex, index);
+        merge_sort(nums, midIndex + 1, r, index);
+        merge(nums, l, midIndex, r, index);
     }
+}
+
+double distance(Point p1, Point p2)
+{
+    return sqrt(pow(p1.x - p2.x, 2) + pow(p1.y - p2.y, 2));
+}
+
+double min(double a, double b)
+{
+    return (a < b) ? a : b;
+}
+
+double brute_force(Point* ps, int size)
+{
+    double min = DBL_MAX;
+    for (int i = 0; i < size; ++i)
+    {
+        for (int j = i + 1; j < size; ++j)
+        {
+            double temp = distance(ps[i], ps[j]);
+            if (temp < min)
+            {
+                min = temp;
+            }
+        }
+    }
+    return min;
+}
+
+double strip_closest(Point* strip, int size, double d)
+{
+    double min = d;
+    merge_sort(strip, 0, size - 1, 1);
+
+    for (int i = 0; i < size; ++i)
+    {
+        for (int j = i + 1; j < size && (strip[j].y - strip[i].y) < min; ++j)
+        {
+            if (distance(strip[i], strip[j]) < min)
+                min = distance(strip[i], strip[j]);
+        }
+    }
+
+    return min;
+}
+
+// A recursive function to find smallest distance, point array is sorted on x coordinate.
+double closest_util(Point* p, int n)
+{
+    if (n <= 3)
+        return brute_force(p, n);
+
+    int mid = n / 2;
+    Point midP = p[mid];
+
+    double dl = closest_util(p, mid);
+    double dr = closest_util(p + mid, n - mid);
+    double d = min(dl, dr);
+
+    Point strip[n];
+    int j = 0;
+    for (int i = 0; i < n; ++i)
+    {
+        if (abs(p[i].x - midP.x) < d)
+            strip[j++] = p[i];
+    }
+
+    return min(d, strip_closest(strip, j, d));
 }
 
 int main()
 {
-    int nums[] = { 186, 419, 83, 408, 304 };
-    int numsSize = sizeof(nums) / sizeof(nums[0]);
-    merge_sort(nums, 0, numsSize - 1);
-    for (int i = 0; i < numsSize; ++i)
-    {
-        cout << nums[i] << " ";
-    }
-    cout << endl;
+    // // Merge sort test
+    // Point points[] = {
+    //     { 2,  3  },
+    //     { 12, 30 },
+    //     { 40, 50 },
+    //     { 5,  1  },
+    //     { 12, 10 },
+    //     { 3,  4  },
+    //     { 1,  2  }
+    // };
+    // int n = sizeof(points) / sizeof(points[0]);
+    // merge_sort(points, 0, n - 1, 0);
+    // for (int i = 0; i < n; ++i)
+    // {
+    //     cout << points[i].x << " " << points[i].y << endl;
+    // }
+
+    Point points[] = {
+        { 2,  3  },
+        { 12, 30 },
+        { 40, 50 },
+        { 5,  1  },
+        { 12, 10 },
+        { 3,  4  },
+        { 1,  2  }
+    };
+    int n = sizeof(points) / sizeof(points[0]);
+    merge_sort(points, 0, n - 1, 0);
+    cout << "The smallest distance is " << closest_util(points, n) << endl;
+
     return 0;
 }
